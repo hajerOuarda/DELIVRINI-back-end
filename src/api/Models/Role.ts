93c989fef213eaa69733c7813cf72e7681b5c7f9
hajer.ouarda <hajer.ouarda@gmail.com>
2022-04-04 15:42:31 +0100

    Added User Role

---
+++ src/api/Models/Role.ts
@@ -0,0 +1,23 @@
+import Sequelize, { Model } from "sequelize";
+import my_Database from "../../database/database";
+
+export class Role extends Model {
+  id!: string;
+  roleName!: string;
+}
+
+Role.init(
+  {
+    roleName: {
+      type: Sequelize.ENUM,
+      values: ["admin", "client", "chef", "deliveryMan"],
+      unique: "roleName",
+      // defaultValue: "admin",
+    },
+  },
+
+  {
+    sequelize: my_Database,
+    // tableName: "roles",
+  }
+);
